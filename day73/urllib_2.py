022bd8b118b1fa7b32b51a662e5e1f6ff89d1621
disen <610039018@qq.com>
2019-04-17 16:37:58 +0800

    urllib的post请求

--- day73/urllib_2.py
+++ day73/urllib_2.py
@@ -5,9 +5,13 @@ urlretrieve() 下载并存储到文件中
 1） 在请求时，如果添加一个请求头
 2） 如果发起post请求
 """
+import json
 import re
 from urllib.request import urlopen, Request
+from urllib.parse import quote, urlencode
 
+import ssl
+ssl._create_default_https_context = ssl._create_unverified_context
 
 def get(url):
     # 声明请求头
@@ -58,7 +62,54 @@ def get(url):
     # print(html)
     # print(response.getheader('Content-Type'))
 
+def post(url, data, headers):
+    # 实现post请求 + 请求头
+    # 设置Request的data参数时，就是post请求
+    # data是字节码
+    data = urlencode(data)
+    request = Request(url, data.encode('utf-8'), headers)
+
+    # 发起请求
+    resp = urlopen(request)
+    if resp.code == 200:
+        # 响应的数据是json类型
+        resp_data = resp.read()
+
+        # 获取字符集
+        content_type = resp.getheader('Content-Type')
+        s = re.findall(r'charset=(\w+)', content_type)
+        if s:
+            charset = s[0]
+            json_data = resp_data.decode(charset)
+        else:
+            json_data = resp_data.decode()
+
+        print(json_data)
+        means = json.loads(json_data)
+        print(means.get('dict_result').get('simple_means').get('word_means'))
+
 
 if __name__ == '__main__':
-    url = 'http://www.haha56.net/'
-    get(url)
+    # url = 'http://www.haha56.net/'
+    # get(url)
+
+    url = 'https://fanyi.baidu.com/v2transapi'
+    data = {
+        'from': 'en',
+        'to': 'zh',
+        'query': 'apple',
+        'transtype': 'realtime',
+        'simple_means_flag': '3',
+        'sign': '704513.926512',
+        'token': '9fc86fc625f77e503a53653963384464'
+    }
+
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:66.0) Gecko/20100101 Firefox/66.0',
+        'Cookie': 'BAIDUID=C54A17B138600A57FB71E5003D727C91:FG=1; BIDUPSID=C54A17B138600A57FB71E5003D727C91; PSTM=1545292520; BDUSS=J1UzR4TFNVUFZOd3p3OHd4VmxBME1UbGM5V29CRVNIWkQ0N3phVzItUXJFYnRjQVFBQUFBJCQAAAAAAAAAAAEAAACVZ8A3ZGF2aWVfYW5kcm9pZAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAACuEk1wrhJNccm; H_PS_PSSID=1445_21094_28774_28723_28557_28831_28585_26350_28604; BDORZ=B490B5EBF6F3CD402E515D22BCDA1598; delPer=0; PSINO=1; BDRCVFR[feWj1Vr5u3D]=I67x6TjHwwYf0; cflag=13%3A3; BDRCVFR[gltLrB7qNCt]=mk3SLVN4HKm; BDRCVFR[Fc9oatPmwxn]=srT4swvGNE6uzdhUL68mv3; locale=zh; Hm_lvt_64ecd82404c51e03dc91cb9e8c025574=1555488376; Hm_lpvt_64ecd82404c51e03dc91cb9e8c025574=1555488376; from_lang_often=%5B%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%2C%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%5D; to_lang_often=%5B%7B%22value%22%3A%22en%22%2C%22text%22%3A%22%u82F1%u8BED%22%7D%2C%7B%22value%22%3A%22zh%22%2C%22text%22%3A%22%u4E2D%u6587%22%7D%5D; REALTIME_TRANS_SWITCH=1; FANYI_WORD_SWITCH=1; HISTORY_SWITCH=1; SOUND_SPD_SWITCH=1; SOUND_PREFER_SWITCH=1'
+    }
+
+    post(url, data, headers)
+
+
