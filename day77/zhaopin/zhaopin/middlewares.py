414f686fe4dbb38366f980dd04721f255836c43f
disen <610039018@qq.com>
2019-04-24 12:03:07 +0800

    scrapy中自定义selenium中间件

---
+++ day77/zhaopin/zhaopin/middlewares.py
@@ -0,0 +1,153 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your spider middleware
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+import time
+from scrapy import signals
+from scrapy.http import HtmlResponse
+from selenium.webdriver import Chrome
+from selenium.webdriver.support import ui, expected_conditions as ec
+from selenium.webdriver.common.by import By
+
+
+class ZhaopinSpiderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the spider middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    def process_spider_input(self, response, spider):
+        # Called for each response that goes through the spider
+        # middleware and into the spider.
+
+        # Should return None or raise an exception.
+        return None
+
+    def process_spider_output(self, response, result, spider):
+        # Called with the results returned from the Spider, after
+        # it has processed the response.
+
+        # Must return an iterable of Request, dict or Item objects.
+        for i in result:
+            yield i
+
+    def process_spider_exception(self, response, exception, spider):
+        # Called when a spider or process_spider_input() method
+        # (from other spider middleware) raises an exception.
+
+        # Should return either None or an iterable of Response, dict
+        # or Item objects.
+        pass
+
+    def process_start_requests(self, start_requests, spider):
+        # Called with the start requests of the spider, and works
+        # similarly to the process_spider_output() method, except
+        # that it doesn’t have a response associated.
+
+        # Must return only requests (not items).
+        for r in start_requests:
+            yield r
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class ZhaopinDownloaderMiddleware(object):
+    # Not all methods need to be defined. If a method is not defined,
+    # scrapy acts as if the downloader middleware does not modify the
+    # passed objects.
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        # This method is used by Scrapy to create your spiders.
+        s = cls()
+        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+        return s
+
+    # [重点]
+    def process_request(self, request, spider):
+        # Called for each request that goes through the downloader
+        # middleware.
+
+        # Must either:
+        # - return None: continue processing this request
+        # - or return a Response object
+        # - or return a Request object
+        # - or raise IgnoreRequest: process_exception() methods of
+        #   installed downloader middleware will be called
+        return None
+
+    def process_response(self, request, response, spider):
+        # Called with the response returned from the downloader.
+
+        # Must either;
+        # - return a Response object
+        # - return a Request object
+        # - or raise IgnoreRequest
+        return response
+
+    def process_exception(self, request, exception, spider):
+        # Called when a download handler or a process_request()
+        # (from other downloader middleware) raises an exception.
+
+        # Must either:
+        # - return None: continue processing this exception
+        # - return a Response object: stops process_exception() chain
+        # - return a Request object: stops process_exception() chain
+        pass
+
+    def spider_opened(self, spider):
+        spider.logger.info('Spider opened: %s' % spider.name)
+
+
+class SeleniumDownloader(object):
+    """
+    自定义selenium的下载中间件
+    实现网页请求由selenium 下载，不经过scrapy的下载器（Downloader）
+    """
+    def __init__(self):
+        # 创建selenium的浏览器对象
+        # window->   d:/drivers/chromedriver.exe
+        # linux->    /home/xxxx/drivers/chromedriver
+        self.browser = Chrome('/Users/apple/drivers/chromedriver')  # mac
+        self.close_ok = False  # 第一次弹出 窗口，需要点击『我知道了』关闭窗口
+
+    def process_request(self, request, spider):
+        # 将会由selenium的chrome浏览器来请求
+        self.browser.get(request.url)
+
+        if not self.close_ok:
+            # 关闭
+            ok_btn = self.browser.find_element_by_class_name('risk-warning__content').find_element_by_tag_name('button')
+            ok_btn.click()
+
+            self.close_ok = True
+
+        #  等待网页中soupager可以选择（可见）
+        ui.WebDriverWait(self.browser, 60).until(
+            ec.visibility_of_all_elements_located((By.CLASS_NAME, 'soupager')))
+
+        # 向下滚动
+        # 滚动屏幕到底部
+        to_position = 1600
+        for i in range(10):
+            self.browser.execute_script('var q = document.documentElement.scrollTop=%s' % ((i + 1) * to_position))
+            time.sleep(0.5)
+
+        # 获取网页数据
+        html = self.browser.page_source
+
+        return HtmlResponse(url=request.url,
+                            body=html.encode(encoding='utf-8'),
+                            encoding='utf-8')
+
+    def __del__(self):
+        self.browser.quit()
---
+++ day77/zhaopin/zhaopin/spiders/xa_python.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+
+
+class XaPythonSpider(scrapy.Spider):
+    name = 'xa_python'
+    allowed_domains = ['sou.zhaopin.com']
+    start_urls = ['https://sou.zhaopin.com/?jl=854&kw=python&kt=3']
+
+    page = 1
+
+    def parse(self, response):
+        with open('zhaopin.html', 'wb') as f:
+            f.write(response.body)
+
+        # 解析数据
+        jobs_xpath = '//div[@class="contentpile__content__wrapper clearfix"]'
+        job_nodes = response.xpath(jobs_xpath)
+
+        print('--job count-->', len(job_nodes))
+
+        for job_node in job_nodes:
+            """
+            job_a = job_node.select_one('a')
+            job_href = job_a.attrs.get('href')
+            job_name = job_a.select_one('.contentpile__content__wrapper__item__info__box__jobname__title').text
+
+            company_a = job_a.select_one('.company_title')
+            company_href = company_a.attrs.get('href')
+            company_name = company_a.attrs.get('title')
+            """
+            try:
+                job_a = job_node.xpath('./a[1]')[0]
+                job_href = job_a.xpath('./@href').extract_first()
+
+                job_name_xpath = './/span[@class, "contentpile__content__wrapper__item__info__box__jobname__title"]/text()'
+                job_name = job_a.xpath(job_name_xpath).extract_first()
+
+                yield {
+                    'job_href': job_href,
+                    'job_name': job_name
+                }
+            except:
+                pass
+
+        # 请求下一页
+        if self.page < 10:
+            self.page += 1
+            next_url = response.url + '&p=%s' % self.page
+
+            yield Request(next_url, callback=self.parse)
